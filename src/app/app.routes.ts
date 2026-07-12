aadc0c9e5ca4256517b803e99fc4fc6398adf4b3
Yurii Spivak <yura.spivak94@gmail.com>
2026-07-12 22:13:45 +0300

    Add factory method page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'factory-method',
+    pathMatch: 'full',
+  },
+  {
+    path: 'factory-method',
+    loadComponent: () => import('./factory-method/factory-method').then((m) => m.FactoryMethod),
+  },
+];
---
+++ src/app/factory-method/factory-method.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-method',
+  template: `
+    <h1>Factory Method</h1>
+  `,
+})
+export class FactoryMethod {}
